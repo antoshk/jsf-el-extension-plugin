a7f37032535850853a35663738c93b667aa6138c
a_shelkovich <a_shelkovich@todes.by>
2019-08-06 00:29:52 +0300

    Refactoring for IU-2019.2 (latest Ultimate IDEA version) support

--- src/main/java/com/antoshk/ElCompletionContributor.java
+++ src/main/java/com/antoshk/ElCompletionContributor.java
@@ -4,77 +4,87 @@ import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.psi.PsiElement;
+import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ElCompletionContributor extends CompletionContributor {
-
+    
+    private static final Logger LOG = Logger.getLogger(ElCompletionContributor.class);
+    
     @Override
     public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
         String text = parameters.getPosition().getText();
         int caretPosition = parameters.getOffset() - parameters.getPosition().getTextOffset();
-        if (needEnumNameCompletion(text, caretPosition)) {
-            Map<String, String> enums = Utils.collectProperties("enumPlugin.properties");
-            String actual = getActualInput(text, caretPosition);
-            enums.entrySet()
-                    .stream()
-                    .filter(entry -> entry.getKey().startsWith(actual))
-                    .forEach(entry -> result.withPrefixMatcher(actual).addElement(LookupElementBuilder.create(entry.getKey())));
-        } else if (needEnumConstantsCompletion(text, caretPosition)) {
-            String actual = getActualInput(text, caretPosition);
-            String[] nameParts = getNameParts(actual);
-            Map<String, String> enums = Utils.collectProperties("enumPlugin.properties");
-            String className = enums.get(nameParts[0]);
+        String actual = getActualInput(text, caretPosition);
+        Map<Parts, String> enumNameParts = processElTree(parameters, actual);
+        
+        if (enumNameParts.get(Parts.ENUM_NAME) != null && enumNameParts.get(Parts.CONST_NAME) != null) {
+            String className = getEnumName(enumNameParts.get(Parts.ENUM_NAME));
             if (className != null) {
-
                 Utils.extractEnumConstants(className)
-                        .filter(enumConst -> enumConst.getName().startsWith(nameParts[1]))
-                        .forEach(enumConst -> {
-                            LookupElementBuilder builder = LookupElementBuilder.create(enumConst.getName());
-                            String args = Utils.createArgList(enumConst);
-                            if (args.length() > 0) {
-                                builder = builder.withTypeText(args);
-                            }
-                            result.withPrefixMatcher(nameParts[1]).addElement(builder);
-                        });
+                    .filter(enumConst -> enumConst.getName().startsWith(actual))
+                    .forEach(enumConst -> {
+                        LookupElementBuilder builder = LookupElementBuilder.create(enumConst.getName());
+                        String args = Utils.createArgList(enumConst);
+                        if (args.length() > 0) {
+                            builder = builder.withTypeText(args);
+                        }
+                        result.withPrefixMatcher(actual).addElement(builder);
+                    });
             }
+        } else if (enumNameParts.get(Parts.ENUM_NAME) != null) {
+            Map<String, String> enums = Utils.collectProperties("enumPlugin.properties");
+            enums.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().startsWith(actual))
+                .forEach(entry -> result.withPrefixMatcher(actual).addElement(LookupElementBuilder.create(entry.getKey())));
         }
     }
-
-    private boolean needEnumConstantsCompletion(String text, int caretPosition) {
-        if (text.contains("#{") && text.indexOf("#{") < caretPosition) {
-            String actual = getActualInput(text, caretPosition);
-            return actual.contains(".") && actual.indexOf('.') == actual.lastIndexOf('.');
+    
+    private Map<Parts, String> processElTree(CompletionParameters parameters, String actual) {
+        Map<Parts, String> parts = new HashMap<>();
+        
+        PsiElement current = parameters.getPosition();
+        
+        if (!current.getParent().toString().equals("ELExpressionHolder")) {
+            current = current.getParent();
         }
-        return false;
-    }
-
-    private boolean needEnumNameCompletion(String text, int caretPosition) {
-        if (text.contains("#{") && text.indexOf("#{") < caretPosition) {
-            return getActualInput(text, caretPosition).matches("[A-Za-z0-9_]+");
+        
+        if (current.getPrevSibling() != null && current.getPrevSibling().getText().equals(".") && actual.matches("[A-Za-z0-9_]*")) {
+            parts.put(Parts.CONST_NAME, current.getText());
+            Optional.ofNullable(current.getPrevSibling().getPrevSibling()).ifPresent(elem -> parts.put(Parts.ENUM_NAME, elem.getText()));
+        } else if (actual.matches("[A-Za-z0-9_]+")) {
+            parts.put(Parts.ENUM_NAME, current.getText());
         }
-        return false;
+        
+        return parts;
     }
-
+    
+    private String getEnumName(String enumName) {
+        if (enumName != null) {
+            Map<String, String> enums = Utils.collectProperties("enumPlugin.properties");
+            return enums.get(enumName);
+        }
+        return null;
+    }
+    
     private String getActualInput(String text, int caretPosition) {
         int lastSpaceIdx = text.lastIndexOf(" ");
         if (lastSpaceIdx < 0 || lastSpaceIdx > caretPosition) {
-            return text.substring(text.lastIndexOf("{") + 1, caretPosition);
+            return text.substring(0, caretPosition);
         } else {
             return text.substring(text.lastIndexOf(" "), caretPosition).trim();
         }
     }
-
-    private String[] getNameParts(String actual) {
-        String[] nameParts = actual.split("\\.");
-        if (nameParts.length == 2) {
-            return nameParts;
-        } else {
-            return new String[]{nameParts[0], ""};
-        }
+    
+    enum Parts {
+        ENUM_NAME, CONST_NAME;
     }
-
-
+    
 }
 
