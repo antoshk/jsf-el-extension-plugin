fc46db2bcb3837143d1726bf3d23df4227380dbe
a_shelkovich <a_shelkovich@todes.by>
2019-08-12 23:06:21 +0300

    merge with 0.2.1

--- src/main/java/com/antoshk/psi/EnumConstantReference.java
+++ src/main/java/com/antoshk/psi/EnumConstantReference.java
@@ -1,6 +1,5 @@
 package com.antoshk.psi;
 
-import com.intellij.model.SymbolResolveResult;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiEnumConstant;
@@ -9,7 +8,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 public class EnumConstantReference extends PsiReferenceBase<PsiElement> {
     
@@ -23,25 +21,13 @@ public class EnumConstantReference extends PsiReferenceBase<PsiElement> {
     @Nullable
     @Override
     public PsiElement resolve() {
-        //return EnumUtils.findEnumConstant(enumName, constantName, project);
         return constant;
     }
     
     @NotNull
     @Override
     public Object[] getVariants() {
-//        List<LookupElement> variants = new ArrayList<>();
-//        String className = EnumUtils.getEnumFullClassName(enumName, project);
-//        EnumUtils.extractEnumConstants(className, project)
-//            .forEach(enumConst -> {
-//                LookupElementBuilder builder = LookupElementBuilder.create(enumConst);
-//                String args = Utils.createArgList(enumConst);
-//                if (args.length() > 0) {
-//                    builder = builder.withTypeText(args);
-//                }
-//                variants.add(builder);
-//            });
         return new ArrayList<>().toArray();
     }
-
+    
 }
