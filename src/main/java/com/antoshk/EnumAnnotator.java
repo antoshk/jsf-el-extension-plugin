13d8c45d7e7893aaa9087c73dc5128dd8fd0aa3f
a_shelkovich <a_shelkovich@todes.by>
2019-08-15 00:34:48 +0300

    annotations for enums and constants

---
+++ src/main/java/com/antoshk/EnumAnnotator.java
@@ -0,0 +1,52 @@
+package com.antoshk;
+
+import com.intellij.codeInsight.completion.CompletionUtilCore;
+import com.intellij.lang.annotation.Annotation;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiEnumConstant;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class EnumAnnotator implements Annotator {
+    
+    @Override
+    public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
+        
+        if (element.getClass().getSimpleName().equals("ELVariableImpl")) {
+            String value = element.getText();
+            if (value != null) {
+                if (value.contains(CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED)) {
+                    value = value.substring(0, value.indexOf(CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED));
+                }
+                Map<ELUtils.Parts, String> enumNameParts = ELUtils.processElTree(element, value);
+                if (enumNameParts.get(ELUtils.Parts.ENUM_NAME) != null && enumNameParts.get(ELUtils.Parts.CONST_NAME) != null) {
+                    PsiClass enumClass = EnumUtils.findEnum(enumNameParts.get(ELUtils.Parts.ENUM_NAME), element.getProject());
+                    if(enumClass != null) {
+                        Annotation annotation = holder.createInfoAnnotation(element.getPrevSibling().getPrevSibling(), null);
+                        annotation.setTextAttributes(DefaultLanguageHighlighterColors.REASSIGNED_LOCAL_VARIABLE);
+                        PsiEnumConstant constant = EnumUtils.findEnumConstant(enumClass, enumNameParts.get(ELUtils.Parts.CONST_NAME));
+                        if (constant != null) {
+                            annotation = holder.createInfoAnnotation(element, null);
+                            annotation.setTextAttributes(DefaultLanguageHighlighterColors.CONSTANT);
+                        } else {
+                            holder.createErrorAnnotation(element, "Unresolved enum constant name");
+                        }
+                    }
+                } else if (enumNameParts.get(ELUtils.Parts.ENUM_NAME) != null) {
+                    PsiClass enumClass = EnumUtils.findEnum(enumNameParts.get(ELUtils.Parts.ENUM_NAME), element.getProject());
+                    if (enumClass != null) {
+                        Annotation annotation = holder.createInfoAnnotation(element, null);
+                        annotation.setTextAttributes(DefaultLanguageHighlighterColors.REASSIGNED_LOCAL_VARIABLE);
+                    }
+                }
+            }
+        }
+        
+    }
+    
+}
